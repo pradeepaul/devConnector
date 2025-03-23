df0e13d5a5f39fb7a8bd761f6aae7c49c2d91c0d
pradeep <pradeepmstech@gmail.com>
2025-03-23 15:49:48 +0530

    Added Post Route API, Add & Delete post and comment

--- routes/api/user.js
+++ routes/api/user.js
@@ -28,9 +28,7 @@ router.post('/', [
     });
 
     user = new User({ name, email, avatar, password });
-
     const salt = await bcrypt.genSalt(10);
-
     user.password = await bcrypt.hash(password, salt);
 
     await user.save();
